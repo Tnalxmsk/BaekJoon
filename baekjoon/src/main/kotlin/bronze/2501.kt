9f20eae491ad14a501d8cf2bc525d16425ec9fc8
muldaeng <alstnwkd990@naver.com>
2023-09-24 02:31:22 +0900

    test

---
+++ baekjoon/src/main/kotlin/bronze/2501.kt
@@ -0,0 +1,26 @@
+package bronze
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val st = StringTokenizer(readLine())
+
+    val n: Int = st.nextToken().toInt()
+    val k: Int = st.nextToken().toInt()
+
+    var result = 0
+    var count = 0
+
+    for (i in 1..n) {
+        if (n%i == 0) {
+            count++
+        }
+        if (count == k) {
+            result = i
+            break
+        }
+    }
+    println(result)
+}
