1b8a2a88c7d950d0e31b17b2c442c0128948fee8
Tnalxmsk <alstnwkd990@naver.com>
2023-11-01 00:07:10 +0900

    백준 2503 다른 코드

---
+++ baekjoon/src/main/kotlin/bronze/2503.kt
@@ -0,0 +1,18 @@
+package bronze
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var st = StringTokenizer(readLine())
+
+    val n = st.nextToken().toInt()
+    val k = st.nextToken().toInt()
+    println(n)
+    println(k)
+    st = StringTokenizer(readLine())
+    val arr = Array(n) {st.nextToken().toInt()}
+    arr.sort()
+    print(arr[n - k])
+}
