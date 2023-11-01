6eff26d227fc4cc9a80caf57964bbc7bc3c6d5de
Tnalxmsk <alstnwkd990@naver.com>
2023-11-01 11:50:44 +0900

    백준 2562 최댓값

---
+++ baekjoon/src/main/kotlin/bronze/2562.kt
@@ -0,0 +1,20 @@
+package bronze
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var maxIndex = 0
+    var max = 0
+    var num = 0
+    repeat(9) {
+        num = br.readLine().toInt()
+        if (num > max) {
+            max = num
+            maxIndex = it
+        }
+    }
+    println(max)
+    println(maxIndex+1)
+}
