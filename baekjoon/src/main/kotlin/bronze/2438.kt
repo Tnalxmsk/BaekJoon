21470ffa77a6d6c29df819221da22e9977aa3e39
Tnalxmsk <alstnwkd990@naver.com>
2023-11-01 11:50:19 +0900

    백준 2438 별찍기1

---
+++ baekjoon/src/main/kotlin/bronze/2438.kt
@@ -0,0 +1,15 @@
+package bronze
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val number = readLine()
+    val sb = StringBuilder()
+    var result = ""
+    repeat(number.toInt()) {
+        result += "*"
+        sb.append("$result\n")
+    }
+    println(sb.toString())
+}
