08e9313c23b8827a026384ecdc01f0bc3e0f4a1f
Tnalxmsk <alstnwkd990@naver.com>
2023-11-01 15:18:17 +0900

    백준 10950 A+B 5

---
+++ baekjoon/src/main/kotlin/bronze/10952.kt
@@ -0,0 +1,21 @@
+package bronze
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var sb = StringBuilder()
+
+    while (true) {
+        val tk = StringTokenizer(br.readLine())
+        val a = tk.nextToken().toInt()
+        val b = tk.nextToken().toInt()
+        if (a == 0 && b == 0) {
+            break
+        }
+        sb.append("${a+b}\n")
+    }
+    println(sb.toString())
+}
