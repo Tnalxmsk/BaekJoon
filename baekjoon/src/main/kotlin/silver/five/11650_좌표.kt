dbbc466f3504610d44260afcc09520c5062766ee
Tnalxmsk <alstnwkd990@naver.com>
2023-12-01 02:13:33 +0900

    백준 11650 x좌표순 정렬

---
+++ baekjoon/src/main/kotlin/silver/five/11650_좌표.kt
@@ -0,0 +1,19 @@
+package silver.five
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val list = mutableListOf<List<String>>()
+    repeat(readLine().toInt()) {
+        val n = readLine().split(" ")
+        list.add(n)
+    }
+    list.sortBy { it[1].toInt() }
+    list.sortBy { it[0].toInt() }
+    list.forEach { bw.write("${it.joinToString(" ")}\n") }
+    bw.flush()
+}
