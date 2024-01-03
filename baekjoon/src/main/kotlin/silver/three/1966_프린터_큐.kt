36c2abb0008473bec12b236a8b331dc859590447
Tnalxmsk <alstnwkd990@naver.com>
2024-01-03 20:48:28 +0900

    백준 1966 프린터 큐

--- baekjoon/src/main/kotlin/silver/three/1966_프린터_큐.kt
+++ baekjoon/src/main/kotlin/silver/three/1966_프린터_큐.kt
@@ -4,16 +4,31 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import java.util.*
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-    val n = readLine().toCharArray()
-    val sum = (n[0] - n[1]
-    if ((n[0] - n[1]) < 48) {
-        bw.write("${-sum}")
-        bw.flush()
-        return@with
+    for(i in 0..<readLine().toInt()) {
+        val queue: Queue<Pair<Int, Int>> = LinkedList()
+        val case = readLine().split(" ")
+        var j = 0
+        readLine().split(" ").forEach {
+            queue.add(Pair(it.toInt(), j))
+            j++
+        }
+
+        val target = case.last().toInt()
+        var count = 0
+        while (true) {
+            if (queue.first().first == queue.maxOf { it.first }.toInt()) {
+                count++
+                if (queue.poll().second == target) break
+            } else {
+                queue.add(queue.poll())
+                continue
+            }
+        }
+        bw.write("${count}\n")
     }
-    bw.write("$sum")
     bw.flush()
 }
